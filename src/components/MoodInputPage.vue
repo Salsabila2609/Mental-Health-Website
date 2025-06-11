ef08f60e9b6eb9f1a45baa73114124e24d825fc3
zaqueen <ethaflsy@gmail.com>
2025-06-11 20:21:50 +0700

    tambah 1 file

---
+++ src/components/MoodInputPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Mood Input</h1>
+    <p>Catat suasana hati kamu di sini ✨</p>
+    <!-- tambahin form input mood, emoji, catatan, dll -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoodInputPage'
+}
+</script>
+
+<style scoped>
+/* Styling sesuai tema */
+</style>
