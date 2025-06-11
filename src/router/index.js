660dd53d57f579354608132db66288f818fb4e30
zaqueen <ethaflsy@gmail.com>
2025-06-11 23:39:14 +0700

    mood

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/HelloWorld.vue'
-import About from '../components/AboutPage.vue' // ini nanti kamu buat
+import About from '../components/AboutPage.vue'
+import Mood from '../components/MoodInputPage.vue' // ini nanti kamu buat
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About }
+  { path: '/about', name: 'About', component: About },
+  { path: '/mood', name: 'Mood Tracker', component: Mood }
 ]
 
 const router = createRouter({
